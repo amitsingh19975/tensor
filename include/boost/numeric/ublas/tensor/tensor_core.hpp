7a6a3376775319085647d4421783b5afbbde0359
Ashar <ashar786khan@gmail.com>
2019-06-01 14:25:17 +0530

    final reformat

--- include/boost/numeric/ublas/tensor/tensor_core.hpp
+++ include/boost/numeric/ublas/tensor/tensor_core.hpp
@@ -17,85 +17,84 @@
 
 // Forward the friends;
 
-namespace boost::numeric::ublas{
-    
-    template <class new_type, class tensor_t>
-    auto static_tensor_cast(tensor_t&);
-    
-    template <class new_type, class tensor_t>
-    auto static_tensor_cast(tensor_t&&);
-    
-    template <class new_type, class tensor_t>
-    auto dynamic_tensor_cast(tensor_t&);
-    
-    template <class new_type, class tensor_t>
-    auto dynamic_tensor_cast(tensor_t&&);
-    
-    template <class new_type, class tensor_t>
-    auto reinterpret_tensor_cast(tensor_t&);
-    
-    template <class new_type, class tensor_t>
-    auto reinterpret_tensor_cast(tensor_t&&);
-    
-    
-    template <class TT, class FF, class AA>
+namespace boost::numeric::ublas {
+
+    template<class new_type, class tensor_t>
+    auto static_tensor_cast(tensor_t &);
+
+    template<class new_type, class tensor_t>
+    auto static_tensor_cast(tensor_t &&);
+
+    template<class new_type, class tensor_t>
+    auto dynamic_tensor_cast(tensor_t &);
+
+    template<class new_type, class tensor_t>
+    auto dynamic_tensor_cast(tensor_t &&);
+
+    template<class new_type, class tensor_t>
+    auto reinterpret_tensor_cast(tensor_t &);
+
+    template<class new_type, class tensor_t>
+    auto reinterpret_tensor_cast(tensor_t &&);
+
+
+    template<class TT, class FF, class AA>
     class tensor;
 }
 
 
-namespace boost::numeric::ublas::detail
-{
+namespace boost::numeric::ublas::detail {
 
-template <class T, class F, class A>
-class tensor_core
-{
-    using array_type = A;
-    using layout_type = F;
-    using strides_type = basic_strides<std::size_t, layout_type>;
-    using extents_type = shape;
+    template<class T, class F, class A>
+    class tensor_core {
+        using array_type = A;
+        using layout_type = F;
+        using strides_type = basic_strides<std::size_t, layout_type>;
+        using extents_type = shape;
 
-public:
-    tensor_core() : extents_(), strides_(), data_() {}
+    public:
+        tensor_core() : extents_(), strides_(), data_() {}
 
-    decltype ( auto ) operator() ( size_t index )
-    {
-        return data_[index];
-    }
-    
-    auto const & extents() const {
-        return extents_;
-    }
+        decltype(auto) operator()(size_t index) {
+            return data_[index];
+        }
 
-private:
-    extents_type extents_;
-    strides_type strides_;
-    array_type data_;
+        auto const &extents() const {
+            return extents_;
+        }
 
-    template <class TT, class FF, class AA>
-    friend class ::boost::numeric::ublas::tensor;
-    
-    template <boost::yap::expr_kind K, typename Tuple>
-    friend struct tensor_expression;
-    
-    template <class type, typename AB>
-    friend auto ::boost::numeric::ublas::static_tensor_cast(AB&);
-    
-    template <class type, typename AB>
-    friend auto ::boost::numeric::ublas::static_tensor_cast(AB&&);
-    
-    template <class type, typename AB>
-    friend auto ::boost::numeric::ublas::dynamic_tensor_cast(AB&);
-    
-    template <class type, typename AB>
-    friend auto ::boost::numeric::ublas::dynamic_tensor_cast(AB&&);
-    
-    template <class type, typename AB>
-    friend auto ::boost::numeric::ublas::reinterpret_tensor_cast(AB&);
+    private:
+        extents_type extents_;
+        strides_type strides_;
+        array_type data_;
 
-    template <class type, typename AB>
-    friend auto ::boost::numeric::ublas::reinterpret_tensor_cast(AB&&);
+        template<class TT, class FF, class AA>
+        friend
+        class ::boost::numeric::ublas::tensor;
 
-};
+        template<boost::yap::expr_kind K, typename Tuple>
+        friend
+        struct tensor_expression;
+
+        template<class type, typename AB>
+        friend auto ::boost::numeric::ublas::static_tensor_cast(AB &);
+
+        template<class type, typename AB>
+        friend auto ::boost::numeric::ublas::static_tensor_cast(AB &&);
+
+        template<class type, typename AB>
+        friend auto ::boost::numeric::ublas::dynamic_tensor_cast(AB &);
+
+        template<class type, typename AB>
+        friend auto ::boost::numeric::ublas::dynamic_tensor_cast(AB &&);
+
+        template<class type, typename AB>
+        friend auto ::boost::numeric::ublas::reinterpret_tensor_cast(AB &);
+
+        template<class type, typename AB>
+        friend auto ::boost::numeric::ublas::reinterpret_tensor_cast(AB &&);
+
+    };
 }  // namespace boost::numeric::ublas::detail
 
 #endif

4124958d28b0b721993b927c85ff73fb94d81511
Ashar <ashar786khan@gmail.com>
2019-06-13 10:59:18 +0530

    documented tensor-expression

--- include/boost/numeric/ublas/tensor/tensor_expression.hpp
+++ include/boost/numeric/ublas/tensor/tensor_expression.hpp
@@ -22,34 +22,65 @@
 
 namespace boost::numeric::ublas::detail {
 
+/**
+ * @brief A YAP expression for tensor type.
+ *
+ * @tparam Kind the Kind of Operation represented
+ *
+ * @tparam Tuple the operands to which operation is performed.
+ */
 template <boost::yap::expr_kind Kind, typename Tuple> struct tensor_expression {
   const static boost::yap::expr_kind kind = Kind;
 
   Tuple elements;
-
+  /**
+   * @brief Evaluates the tensor_expression lazily. one index at a time.
+   *
+   * @param i the Index to evaluate
+   *
+   * @return the Evaluated Value of the expression at ith index.
+   */
   BOOST_UBLAS_INLINE decltype(auto) operator()(size_t i) {
     auto nth = boost::yap::transform(*this, transforms::at_index{i});
     return boost::yap::evaluate(nth);
   }
+  //  todo (coder3101) : Make eval() and eval_to() based on device.
 
-  // todo(coder3101) : Make eval() and eval_to() based on device.
-
+  /**
+   * @brief Completely evaluated this expression and returns a tensor.
+   *
+   * @tparam T the data type to use for resulting tensor.
+   *
+   * @tparam F the storage format to use for resulting tensor.
+   *
+   * @tparam A the array type to use for resulting tensor.
+   *
+   * @return The tensor which contains the values of evaluated expresssion.
+   */
   template <class T, class F = ::boost::numeric::ublas::first_order,
             class A = std::vector<T, std::allocator<T>>>
   auto eval() const {
     ::boost::numeric::ublas::tensor<T, F, A> result;
-
     auto shape_expr = boost::yap::transform(*this, transforms::get_extents{});
-
     result.extents_ = shape_expr;
     result.strides_ = basic_strides<std::size_t, F>{shape_expr};
     result.data_.resize(shape_expr.product());
-
+#pragma omp parallel for
     for (auto i = 0u; i < shape_expr.product(); i++)
       result.data_[i] = this->operator()(i);
-
     return result;
   }
+  /**
+   * @brief Completely evaluates this expression and fills values into target.
+   *
+   * @tparam T the data-type of target tensor (deduced)
+   *
+   * @tparam F the format-type of target tensor (deduced)
+   *
+   * @tparam A the  array type of target tensor (deduced)
+   *
+   * @param[out] target the resulting tensor.
+   */
 
   template <class T, class F, class A>
   void eval_to(::boost::numeric::ublas::tensor<T, F, A> &target) {
@@ -61,6 +92,13 @@ template <boost::yap::expr_kind Kind, typename Tuple> struct tensor_expression {
     for (auto i = 0u; i < shape_expr.product(); i++)
       target.data_[i] = this->operator()(i);
   }
+
+  /**
+   * @brief Implictly converts this tensor_expression to a bool type.
+   *
+   * @note This conversion throws a runtime_error if the expression does not
+   * contain any logical operator.
+   */
   operator bool() {
     boost::numeric::ublas::detail::transforms::expr_has_logical_operator e;
     boost::yap::transform(*this, e);

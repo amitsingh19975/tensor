dd8ba3816a958b605ed5d4d8f2fb932f1e1dc766
Ashar <ashar786khan@gmail.com>
2019-06-22 16:13:08 +0530

    disable branch-coverage

--- .ci/report_coverage.sh
+++ .ci/report_coverage.sh
@@ -6,14 +6,14 @@
 if [ $CXXSTD == '17' ] 
     then 
     # Capture all the coverge reports
-    lcov --rc lcov_branch_coverage=1 --directory $BOOST_ROOT --capture --output-file coverage.info 
+    lcov --directory $BOOST_ROOT --capture --output-file coverage.info
     
     # Remove all unwanted coverages libs. 
     # Boost.uBLAS depends uses many internal boost libs, we don't want them to be in coverage.
     
-    lcov --rc lcov_branch_coverage=1 --extract coverage.info "*/boost/numeric/ublas/*" "*/libs/numeric/ublas/*" --output-file coverage.info
+    lcov --extract coverage.info "*/boost/numeric/ublas/*" "*/libs/numeric/ublas/*" --output-file coverage.info
     
-    lcov --rc lcov_branch_coverage=1 --list coverage.info
+    lcov --list coverage.info
 else
     echo "Skipping Coverage Reporting as CXXSTD is 11"
 fi

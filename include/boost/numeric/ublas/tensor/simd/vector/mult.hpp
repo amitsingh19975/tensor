74840a96a7e7118cd901b15e6afc14a319c4ccf8
Amit Singh <amitsingh19975@gmail.com>
2020-05-19 19:54:26 +0530

    adding mtv using simd

--- include/boost/numeric/ublas/tensor/simd/vector/mult.hpp
+++ include/boost/numeric/ublas/tensor/simd/vector/mult.hpp
@@ -2,7 +2,7 @@
 #define VECTOR_MULT_HPP
 
 #include <boost/numeric/ublas/tensor/simd/detail/simd_helper.hpp>
-
+#include <omp.h>
 
 void print(__m256 d, int N ){
     std::cout<<"ymm"<<N<<"[ ";
@@ -15,11 +15,17 @@ void print(__m256 d, int N ){
 namespace boost::numeric::ublas::simd{
     
     struct partition{
-        inline constexpr size_t k() const noexcept{
-            return 48;
+        inline constexpr size_t k(size_t) const noexcept{
+            return 8;
         }
-        inline constexpr size_t m() const noexcept{
-            return 88;
+        inline constexpr size_t m(size_t M) const noexcept{
+            if( M > 2000 ){
+                auto nm = M / 4;
+                auto rem = nm % 8;
+                return nm - rem;
+            }else{
+                return M;
+            }
         }
         inline constexpr size_t n() const noexcept{
             return 1;
@@ -29,9 +35,9 @@ namespace boost::numeric::ublas::simd{
         // BOOST_UBLAS_TENSOR_ALWAYS_INLINE
         // float const* pack(float const* a, SizeType const* na, SizeType const* wa, SizeType const offset = 0) noexcept{
         //     auto ai = a;
-        //     auto ci = buffA + off;
+        //     auto ci = buffA + offset;
 
-        //     for( auto i = na[0]; i > 0 ;  --i ){
+        //     for( auto volatile i = na[1]; i > 0 ;  --i ){
         //         _mm_prefetch(ai, _MM_HINT_T0);
         //         _mm_prefetch(ci, _MM_HINT_T0);
         //         auto aj = ai;
@@ -47,7 +53,7 @@ namespace boost::numeric::ublas::simd{
         //     align_allocator<float>{}.deallocate(buffA,0);
         // }
         
-        // float* buffA{ align_allocator<float>{}.allocate( k() * m() ) };
+        // float* buffA{ align_allocator<float>{}.allocate( k() * m() * 16 ) };
     };
 
     struct Kernel{
@@ -79,7 +85,7 @@ namespace boost::numeric::ublas::simd{
                 SizeType const ntc[] = { M , 1};
 
 
-                kernel_helper_asm(
+                kernel_helper(
                     cj, ntc, wc,
                     aj, nta, wa,
                     bj, ntb, wb
@@ -117,82 +123,91 @@ namespace boost::numeric::ublas::simd{
             float const* a, SizeType const* na, SizeType const* wa,
             float const* b, SizeType const* nb, SizeType const* wb
         ) const noexcept{
-            
-            // std::cout<<std::endl;
-            // for(auto i = 0; i < nc[0]; ++i){
-            //     for(auto j = 0; j < nc[1]; ++j){
-            //         std::cout<<c[i * wc[0] + j * wc[1]]<<' ';
-            //     }
-            //     std::cout<<std::endl;
-            // }
-            
-            // std::cout<<std::endl;
-            // for(auto i = 0; i < na[0]; ++i){
-            //     for(auto j = 0; j < na[1]; ++j){
-            //         std::cout<<a[i * wa[0] + j * wa[1]]<<' ';
-            //     }
-            //     std::cout<<std::endl;
-            // }
-            
-            // std::cout<<std::endl;
-            // for(auto i = 0; i < nb[0]; ++i){
-            //     for(auto j = 0; j < nb[1]; ++j){
-            //         std::cout<<b[i * wb[0] + j * wb[1]]<<' ';
-            //     }
-            //     std::cout<<std::endl;
-            // }
-
-            __m256 ymm[16] = {_mm256_setzero_ps()};
-
             auto k_iter = na[1] / 8;
             auto k_rem = na[1] % 8;
 
-            prefetch(c,wc[0]);
-            for( auto k = 0ul; k < k_iter; ++k ){
-                ymm[0] = _mm256_load_ps(a + wa[1] * 0);
-                ymm[1] = _mm256_load_ps(a + wa[1] * 1);
-                ymm[2] = _mm256_load_ps(a + wa[1] * 2);
-                ymm[3] = _mm256_load_ps(a + wa[1] * 3);
-                ymm[4] = _mm256_load_ps(a + wa[1] * 4);
-                ymm[5] = _mm256_load_ps(a + wa[1] * 5);
-                ymm[6] = _mm256_load_ps(a + wa[1] * 6);
-                ymm[7] = _mm256_load_ps(a + wa[1] * 7);
-                ymm[8] = _mm256_load_ps(b + wb[1] * 0);
-                ymm[9] = _mm256_moveldup_ps(ymm[8]);
-                ymm[10] = _mm256_movehdup_ps(ymm[8]);
+            _mm_prefetch(c,_MM_HINT_T0);
+            
+            auto ak = a;
+            auto bk = b;
+            auto ck = c;
 
-                // _mm_prefetch(a + wa[1] * 8, _MM_HINT_T0);
-                // _mm_prefetch(b + wa[1] * 8, _MM_HINT_T0);
+            asm volatile (R"(
+                .align 5
+                movq %[a], %%rax
+                movq %[b], %%rbx
+                movq %[c], %%rcx
+                movq %[wa], %%r9
+                movq %[k_it], %%r10
+                movq %[k_r], %%r13
+                movq %[wa], %%r11
+                movq %[wa], %%r12
+                imul $32, %%r11
+                imul $12, %%r12
 
-                ymm[15] = _mm256_fmadd_ps(ymm[0], _mm256_permute2f128_ps(ymm[9],ymm[9],_MM_SHUFFLE(0,0,0,0)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[1], _mm256_permute2f128_ps(ymm[10],ymm[10],_MM_SHUFFLE(0,0,0,0)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[2], _mm256_permute2f128_ps(ymm[9],ymm[9],_MM_SHUFFLE(1,1,1,1)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[3], _mm256_permute2f128_ps(ymm[10],ymm[10],_MM_SHUFFLE(1,1,1,1)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[4], _mm256_permute2f128_ps(ymm[9],ymm[9],_MM_SHUFFLE(2,2,2,2)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[5], _mm256_permute2f128_ps(ymm[10],ymm[10],_MM_SHUFFLE(2,2,2,2)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[6], _mm256_permute2f128_ps(ymm[9],ymm[9],_MM_SHUFFLE(3,3,3,3)), ymm[15]);
-                ymm[15] = _mm256_fmadd_ps(ymm[7], _mm256_permute2f128_ps(ymm[10],ymm[10],_MM_SHUFFLE(3,3,3,3)), ymm[15]);
+                testq $0,%%r10
+                jne LKERNEL8x8LOOPZERO%=
+                vmovaps (%%rcx), %%ymm15
+            LKERNEL8x8LOOP%=:
+                prefetcht0 (%%rax,%%r11)
+                prefetcht0 32(%%rbx)
+
+                vbroadcastss (%%rbx), %%ymm11
+                vbroadcastss 4(%%rbx), %%ymm12
+                vbroadcastss 8(%%rbx), %%ymm13
+                vbroadcastss 12(%%rbx), %%ymm14
+
+                vfmadd231ps (%%rax), %%ymm11, %%ymm15
+                vfmadd231ps (%%rax, %%r9, 4), %%ymm12, %%ymm15
+                vfmadd231ps (%%rax, %%r9, 8), %%ymm13, %%ymm15
                 
+                addq %%r12, %%rax
+                vfmadd231ps (%%rax), %%ymm14, %%ymm15
 
-                b += wb[1] * 8;
-                a += wa[1] * 8;
-            }
-            // std::cout<<na[1]<<'\n';
-            // for(auto k = 0; k < k_rem; ++k){
-            //     ymm[k] = _mm256_load_ps(a);
-            //     ymm[k + 1] = _mm256_broadcast_ss(b);
-            //     _mm_prefetch(a + wa[1], _MM_HINT_T0);
-            //     _mm_prefetch(b + wb[1], _MM_HINT_T0);
+                vbroadcastss 16(%%rbx), %%ymm11
+                vbroadcastss 20(%%rbx), %%ymm12
+                vbroadcastss 24(%%rbx), %%ymm13
+                vbroadcastss 28(%%rbx), %%ymm14
 
-            //     ymm[8] = _mm256_fmadd_ps(ymm[k], ymm[k + 1], ymm[8]);
-            //     // PRINT(k);
-            //     // PRINT(k + 1);
-            //     // PRINT(8);
-            //     a += wa[1];
-            //     b += wb[1];
-            // }
+                vfmadd231ps (%%rax, %%r9, 4), %%ymm11, %%ymm15
+                vfmadd231ps (%%rax, %%r9, 8), %%ymm12, %%ymm15
+                
+                addq %%r12, %%rax
+                vfmadd231ps (%%rax), %%ymm13, %%ymm15
+                vfmadd231ps (%%rax, %%r9, 4), %%ymm14, %%ymm15
+                
+                leaq (%%rax, %%r9, 8), %%rax
+                addq $32, %%rbx
 
-            _mm256_storeu_ps(c,ymm[15]);
+                decq %%r10
+                jne LKERNEL8x8LOOP%=
+                
+                #testq $0,%%r13
+                #jne LKERNEL8x8LOOPZERO%=
+
+            LKERNEL8x8LOOPREM%=:
+
+
+
+                #jne LKERNEL8x8LOOPREM%=
+
+                #jmp LKERNEL8x8LOOPZERO%=
+            LKERNEL8x8LOOPZERO%=:
+                vmovaps %%ymm15, (%%rcx)
+
+            )"
+                : [c] "=m" (c)
+                : [a] "m" (a)
+                , [b] "m" (b)
+                , [k_it] "r" (k_iter)
+                , [k_r] "r" (k_rem)
+                , [wa] "m" (wa[1])
+                : "%ymm0", "%ymm1", "%ymm2", "%ymm3"
+                , "%ymm4", "%ymm5", "%ymm6", "%ymm7"
+                , "%ymm8", "%ymm9", "%ymm10", "%ymm11"
+                , "%ymm12", "%ymm13", "%ymm14", "%ymm15"
+                , "%rax", "%rbx", "%rcx", "%r9", "%r10", "%r11", "%r12", "%r13"
+            );
         }
         template<typename SizeType>
         BOOST_UBLAS_TENSOR_ALWAYS_INLINE
@@ -235,7 +250,6 @@ namespace boost::numeric::ublas::simd{
             auto ak = a;
             auto bk = b;
             auto ck = c;
-            // __m256 ymm0, ymm1,ymm2,ymm3,ymm4,ymm5,ymm6,ymm7,ymm8,ymm9;
 
             asm volatile (R"(
                 .align 5
@@ -307,16 +321,6 @@ namespace boost::numeric::ublas::simd{
 
             )"
                 : [c] "=m" (c)
-                // , [y0] "=m" (ymm0)
-                // , [y1] "=m" (ymm1)
-                // , [y2] "=m" (ymm2)
-                // , [y3] "=m" (ymm3)
-                // , [y4] "=m" (ymm4)
-                // , [y5] "=m" (ymm5)
-                // , [y6] "=m" (ymm6)
-                // , [y7] "=m" (ymm7)
-                // , [y8] "=m" (ymm8)
-                // , [cym] "=m" (ymm9)
                 : [a] "m" (a)
                 , [b] "m" (b)
                 , [k_it] "r" (k_iter)
@@ -328,21 +332,6 @@ namespace boost::numeric::ublas::simd{
                 , "%ymm12", "%ymm13", "%ymm14", "%ymm15"
                 , "%rax", "%rbx", "%rcx", "%r9", "%r10", "%r11", "%r12", "%r13"
             );
-
-            // std::cout<<"----------------------\n";
-
-            // PRINT(0);
-            // PRINT(1);
-            // PRINT(2);
-            // PRINT(3);
-            // PRINT(4);
-            // PRINT(5);
-            // PRINT(6);
-            // PRINT(7);
-            // PRINT(8);
-            // PRINT(9);
-
-            // std::cout<<"----------------------\n";
 
         }
     };
@@ -429,16 +418,16 @@ namespace boost::numeric::ublas::simd{
         auto ci = c;
 
         
+        auto const K = par.k(na[1]);
+        auto const M = par.m(na[0]);
 
-        auto wa_1 = wa[1] * par.k();
-        auto wa_0 = wa[0] * par.m();
-        auto wb_0 = wb[0] * par.k();
-        auto wc_0 = wc[0] * par.m();
+        auto wa_1 = wa[1] * K;
+        auto wa_0 = wa[0] * M;
+        auto wb_0 = wb[0] * K;
+        auto wc_0 = wc[0] * M;
 
-        auto const K = par.k();
-        auto const M = par.m();
 
-        #pragma omp parallel for schedule(dynamic,1)
+        #pragma omp parallel for schedule(dynamic)
         for( auto i = 0ul; i < na[0]; i += M ){
             auto const ib = std::min(na[0] - i, M);
 
@@ -453,11 +442,13 @@ namespace boost::numeric::ublas::simd{
                 auto bii = bk;
                 auto cii = ck;
 
+
                 SizeType const nta[] = { ib, kb};
 
                 SizeType const ntb[] = { kb, 1};
 
                 SizeType const ntc[] = { ib, 1};
+
                 {
                     ker(
                         ck, ntc, wc,
